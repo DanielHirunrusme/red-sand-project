a49ddc5310ec322478f425c9b0f035b02b3b9131
Matthew Tsang <MattheTsang@Matthews-MacBook-Pro-2.local>
2017-09-01 16:08:01 -0400

    setup custom posts, community and prints

--- wp-content/themes/red-sand-project/functions.php
+++ wp-content/themes/red-sand-project/functions.php
@@ -120,6 +120,9 @@ function html5blank_styles()
 
     wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
+
+    wp_register_style('html5blank', get_template_directory_uri() . '/assets/main.min.css', array(), '1.0', 'all');
+    wp_enqueue_style('rsp_css'); // Enqueue it!
 }
 
 // Register HTML5 Blank Navigation
@@ -398,23 +401,23 @@ add_shortcode('html5_shortcode_demo_2', 'html5_shortcode_demo_2'); // Place [htm
 // Create 1 Custom Post type for a Demo, called HTML5-Blank
 function create_post_type_html5()
 {
-    register_taxonomy_for_object_type('category', 'html5-blank'); // Register Taxonomies for Category
-    register_taxonomy_for_object_type('post_tag', 'html5-blank');
-    register_post_type('html5-blank', // Register Custom Post Type
+    register_taxonomy_for_object_type('category', 'prints'); // Register Taxonomies for Category
+    register_taxonomy_for_object_type('post_tag', 'prints');
+    register_post_type('prints', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('HTML5 Blank Custom Post', 'html5blank'), // Rename these to suit
-            'singular_name' => __('HTML5 Blank Custom Post', 'html5blank'),
-            'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New HTML5 Blank Custom Post', 'html5blank'),
-            'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit HTML5 Blank Custom Post', 'html5blank'),
-            'new_item' => __('New HTML5 Blank Custom Post', 'html5blank'),
-            'view' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'view_item' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'search_items' => __('Search HTML5 Blank Custom Post', 'html5blank'),
-            'not_found' => __('No HTML5 Blank Custom Posts found', 'html5blank'),
-            'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'html5blank')
+            'name' => __('Prints', 'prints'), // Rename these to suit
+            'singular_name' => __('Prints', 'prints'),
+            'add_new' => __('Add New', 'prints'),
+            'add_new_item' => __('Add New Prints Post', 'prints'),
+            'edit' => __('Edit', 'prints'),
+            'edit_item' => __('Edit Prints Post', 'prints'),
+            'new_item' => __('New Prints Post', 'posts'),
+            'view' => __('View Prints Post', 'posts'),
+            'view_item' => __('View Prints Post', 'posts'),
+            'search_items' => __('Search Prints Post', 'posts'),
+            'not_found' => __('No Prints Posts found', 'posts'),
+            'not_found_in_trash' => __('No Prints Posts found in Trash', 'posts')
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
@@ -431,7 +434,48 @@ function create_post_type_html5()
             'category'
         ) // Add Category and Post Tags support
     ));
+
+    register_taxonomy_for_object_type('category', 'community'); // Register Taxonomies for Category
+    register_taxonomy_for_object_type('post_tag', 'community');
+    register_post_type('community', // Register Custom Post Type
+        array(
+        'labels' => array(
+            'name' => __('Community', 'community'), // Rename these to suit
+            'singular_name' => __('Community', 'community'),
+            'add_new' => __('Add New', 'community'),
+            'add_new_item' => __('Add New Participant', 'community'),
+            'edit' => __('Edit', 'community'),
+            'edit_item' => __('Edit Participant Post', 'community'),
+            'new_item' => __('New Participants Post', 'community'),
+            'view' => __('View Participants Post', 'community'),
+            'view_item' => __('View Participants Post', 'community'),
+            'search_items' => __('Search Participants Post', 'wposts'),
+            'not_found' => __('No Participants Posts found', 'posts'),
+            'not_found_in_trash' => __('No Participants Posts found in Trash', 'posts')
+        ),
+        'public' => true,
+        'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
+        'has_archive' => true,
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail'
+        ), // Go to Dashboard Custom HTML5 Blank post for supports
+        'can_export' => true, // Allows export in Tools > Export
+        'taxonomies' => array(
+            'post_tag',
+            'category'
+        ) // Add Category and Post Tags support
+    ));
+
 }
+
+function remove_menus(){
+  remove_menu_page( 'edit-comments.php' );          //Comments
+  
+}
+add_action( 'admin_menu', 'remove_menus' );
 
 /*------------------------------------*\
 	ShortCode Functions
